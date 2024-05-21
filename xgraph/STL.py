6b5e25f0351827896c42af0f5585b527649bab63
NAKANISHI\sygha <syghabc@gmail.com>
2024-05-21 21:24:51 +0900

    STLのバイナリデータ読み込み機能追加

--- xgraph/STL.py
+++ xgraph/STL.py
@@ -1,6 +1,8 @@
 import numpy as np
 from xgraph.core import stl
 import sys
+import os
+import struct
 
 def read_ASCII(filename):
     Info = []
@@ -29,6 +31,51 @@ def read_ASCII(filename):
             stls.append(stl(facet_normals, triangles, info["name"]))
     
     return tuple(stls)
+
+def read_Binary(filename):
+    Info = []
+    stls = []
+
+    filesize = os.path.getsize(filename)
+    with open(filename, "rb") as file:
+        while filesize > 0:
+            Info.append({"name" : file.read(80).decode(), "facet_normals" : [], "vertice" : []})
+            filesize -= 80
+            tri_num = struct.unpack("I", file.read(4))[0] #triangle number
+            filesize -= 4
+            for _ in range(tri_num):
+                x = struct.unpack("f", file.read(4))[0]
+                y = struct.unpack("f", file.read(4))[0]
+                z = struct.unpack("f", file.read(4))[0]
+                filesize -= 12
+                Info[-1]["facet_normals"].append(np.array([x, y, z]))
+
+                tri1_x = struct.unpack("f", file.read(4))[0]
+                tri1_y = struct.unpack("f", file.read(4))[0]
+                tri1_z = struct.unpack("f", file.read(4))[0]
+                Info[-1]["vertice"].append(np.array([[tri1_x, tri1_y, tri1_z]]))
+
+                tri2_x = struct.unpack("f", file.read(4))[0]
+                tri2_y = struct.unpack("f", file.read(4))[0]
+                tri2_z = struct.unpack("f", file.read(4))[0]
+                Info[-1]["vertice"].append(np.array([[tri2_x, tri2_y, tri2_z]]))
+
+                tri3_x = struct.unpack("f", file.read(4))[0]
+                tri3_y = struct.unpack("f", file.read(4))[0]
+                tri3_z = struct.unpack("f", file.read(4))[0]
+                Info[-1]["vertice"].append(np.array([[tri3_x, tri3_y, tri3_z]]))
+
+                filesize -= 36
+                unknown = file.read(2)
+                filesize -= 2
+    
+        for info in Info:
+            facet_normals = np.stack(info["facet_normals"])
+            triangles = np.stack(info["vertice"]).reshape((-1,3,3))
+            stls.append(stl(facet_normals, triangles, info["name"]))
+    
+    return tuple(stls)
+    
 
 def write_ASCII(stl, filename):
     with open(filename, "w") as file:
